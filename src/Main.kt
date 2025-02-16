4cc740bf86a839f258e59a4d2cd3d57a820340e4
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-16 18:36:39 +0300

    Finished all parts: created a class that takes info of a book and prints its info

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+
+    var myBook = Book("Harry Potter and the Chamber of Secrets", "JK Rowling", 1998, "Fiction");
+    myBook.getBookInfo();
+}
+
+class Book(var title: String = "", var author: String = "", var yearPublished: Int = 0, var genre: String = "") {
+    fun getBookInfo() {
+        println("$title by $author (${yearPublished}) (${genre})");
+    }
 }
